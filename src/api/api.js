6f5a28eb46bc2096c0135bc57acdd4f9365bb5d5
rizkon86system <rizkon86system@gmail.com>
2025-06-29 16:44:18 +0700

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://203.194.114.211:3000/api",
 });
 
 API.interceptors.request.use((config) => {
